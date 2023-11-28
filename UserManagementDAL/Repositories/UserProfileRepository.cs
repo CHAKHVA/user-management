839b12193c611edc4096eb92e1e1a001149ef470
Aleksandre Chakhvashvili <aleksandre.chakhvashvili24@gmail.com>
2023-11-29 00:19:29 +0400

    create repositories in dal

---
+++ UserManagementDAL/Repositories/UserProfileRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserManagementDAL.Data;
+using UserManagementDAL.Entities;
+using UserManagementDAL.Interfaces;
+
+namespace UserManagementDAL.Repositories;
+
+public class UserProfileRepository : Repository<UserProfile>, IUserProfileRepository
+{
+    public UserProfileRepository(UserManagementContext context) : base(context)
+    {
+    }
+
+    public async Task<IEnumerable<UserProfile>> GetActiveProfiles()
+    {
+        return await _context.UserProfiles.Where(up => up.IsActive).ToListAsync();
+    }
+}
+
---
+++ UserManagementDAL/Repositories/UserRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserManagementDAL.Data;
+using UserManagementDAL.Entities;
+using UserManagementDAL.Interfaces;
+
+namespace UserManagementDAL.Repositories;
+
+public class UserRepository : Repository<User>, IUserRepository
+{
+    public UserRepository(UserManagementContext context) : base(context)
+    {
+    }
+
+    public async Task<User> GetUserByEmail(string email)
+    {
+        return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+    }
+}
+
