8b94b964162bae55b512f40486e7c28f96fd31f1
Aleksandre Chakhvashvili <aleksandre.chakhvashvili24@gmail.com>
2023-11-29 00:59:56 +0400

    create services in bll

---
+++ UserManagementBLL/Services/UserProfileService.cs
@@ -0,0 +1,21 @@
+﻿using UserManagementBLL.Interfaces;
+using UserManagementDAL.Entities;
+using UserManagementDAL.Interfaces;
+
+namespace UserManagementBLL.Services;
+
+public class UserProfileService : Service<UserProfile>, IUserProfileService
+{
+    private readonly IUserProfileRepository _userProfileRepository;
+
+    public UserProfileService(IUserProfileRepository userProfileRepository) : base(userProfileRepository)
+    {
+        _userProfileRepository = userProfileRepository;
+    }
+
+    public async Task<IEnumerable<UserProfile>> GetActiveProfilesAsync()
+    {
+        return await _userProfileRepository.GetActiveProfilesAsync();
+    }
+}
+
---
+++ UserManagementBLL/Services/UserService.cs
@@ -0,0 +1,21 @@
+﻿using UserManagementBLL.Interfaces;
+using UserManagementDAL.Entities;
+using UserManagementDAL.Interfaces;
+
+namespace UserManagementBLL.Services;
+
+public class UserService : Service<User>, IUserService
+{
+    private readonly IUserRepository _userRepository;
+
+    public UserService(IUserRepository userRepository) : base(userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    public async Task<User> GetUserByEmailAsync(string email)
+    {
+        return await _userRepository.GetUserByEmailAsync(email);
+    }
+}
+
